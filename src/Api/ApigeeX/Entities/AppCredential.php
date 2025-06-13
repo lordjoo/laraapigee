49bdd32a40290557eec101e59c3c08e0eae3c875
Youssef <youssef.afify122@gmail.com>
2025-06-13 17:20:16 +0300

    add apigge x app key service

---
+++ src/Api/ApigeeX/Entities/AppCredential.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Lordjoo\LaraApigee\Api\ApigeeX\Entities;
+
+use Lordjoo\LaraApigee\Api\Edge\Entities\AppCredential as BaseAppCredentials;
+
+class AppCredential extends BaseAppCredentials
+{
+}
